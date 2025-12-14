0912c0ab7ef10450987673ba71a4a142697519eb
uyennl0503 <uyennlda170024@fpt.edu.vn>
2025-12-14 15:32:22 +0700

    Fix careless

--- src/main/java/com/spring/fit/backend/common/enums/FulfillmentStatus.java
+++ src/main/java/com/spring/fit/backend/common/enums/FulfillmentStatus.java
@@ -3,7 +3,7 @@ package com.spring.fit.backend.common.enums;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum FulfillmentStatus {
-    UNFULFILLED(" "),
+    UNFULFILLED("UNFULFILLED"),
     FULFILLED("FULFILLED"),
     PARTIALLY_FULFILLED("PARTIALLY_FULFILLED"),
     CANCELLED("CANCELLED");
